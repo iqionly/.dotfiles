e16bfda6f01aa500b2934aa67218ab8d216ca351
iqionly <mochammadrizkyashyari@gmail.com>
2026-01-25 14:49:56 +0700

    Delete Harpoon, VIM does have similiiar builtin function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,7 @@ vim.keymap.set('v', '<S-Tab>', '<gv')
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
@@ -18,3 +18,6 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gr", function()
   require("telescope.builtin").lsp_references()
 end, { desc = "LSP references" });
+
+vim.keymap.set('n', '<C-P>', '<C-I>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-\'', ':jumps<CR>', { noremap = true, silent = true })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,38 @@
 return {
-   "nvim-telescope/telescope.nvim",
-   cmd = "Telescope",
-   version = false,
-   opts = {
-       defaults = {
-           layout_strategy = "horizontal",
-           layout_config = {
-               horizontal = {
-                   prompt_position = "bottom",
-                   width = { padding = 0 },
-                   height = { padding = 0 },
-                   preview_width = 0.5,
-               },
-           },
-           sorting_strategy = "descending",
-       },
-   },
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    version = false,
+    dependencies = {
+        { 
+            "nvim-telescope/telescope-live-grep-args.nvim" ,
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            version = "^1.0.0",
+        },
+    },
+    config = function()
+        local telescope = require("telescope")
+
+        -- first setup telescope
+        telescope.setup({
+            -- your config
+        })
+
+        -- then load the extension
+        telescope.load_extension("live_grep_args")
+    end,
+    opts = {
+        defaults = {
+            layout_strategy = "horizontal",
+            layout_config = {
+                horizontal = {
+                    prompt_position = "bottom",
+                    width = { padding = 0 },
+                    height = { padding = 0 },
+                    preview_width = 0.5,
+                },
+            },
+            sorting_strategy = "descending",
+        },
+    },
 };
