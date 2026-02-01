70abbc646c265eb1dcb4473279191d6e5f30fd79
iqionly <mochammadrizkyashyari@gmail.com>
2026-02-01 14:40:19 +0700

    Enable signature blink-cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -47,7 +47,10 @@ return {
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+
+        -- Enabled Signature Help Float
+        signature = { enabled = true },
     },
     opts_extend = { "sources.default" }
 };
