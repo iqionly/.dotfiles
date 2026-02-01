0554d1ac0dc7a9878649ce42b5c19755da442e9b
iqionly <mochammadrizkyashyari@gmail.com>
2026-02-01 14:44:19 +0700

    Enable auto open document completion blink-cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -34,7 +34,13 @@ return {
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = {
+            documentation = {
+                auto_show = false,
+                auto_show_delay_ms = 500,
+            },
+        },
+
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -48,9 +54,6 @@ return {
         --
         -- See the fuzzy documentation for more information
         fuzzy = { implementation = "prefer_rust_with_warning" },
-
-        -- Enabled Signature Help Float
-        signature = { enabled = true },
     },
     opts_extend = { "sources.default" }
 };
